e8c8d10b2d99de461068b309f7f20c890d20214f
tonythecoder1 <tonypensais@hotmail.com>
2026-04-19 03:21:46 +0200

    Add timesheets MVC app with PostgreSQL support

---
+++ Timesheets.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Timesheets.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
